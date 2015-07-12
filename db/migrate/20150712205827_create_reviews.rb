584f00cac0530ec66d6658e8b3c60f3c50140f35
Nancy Wood <nancywood13@gmail.com>
2015-07-12 17:03:56 -0400

    fix error with review table in migration due to typo

---
+++ db/migrate/20150712205827_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.text :comment
+      t.integer :product_id
+      t.integer :user_id
+    end
+  end
+end
