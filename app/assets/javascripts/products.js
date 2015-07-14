57227451e7576271762b0fc05317e11036281d5a
Nancy Wood <nancywood13@gmail.com>
2015-07-14 17:32:21 -0400

    debug ajax search

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on('ready page:load', function() {
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+      $.getScript('/products?search=' + searchValue);
+      // .done(function(data){
+      //   console.log(data);
+      //   $('#products').html(data);
+      // });
+  });
+});
